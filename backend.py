0589f678daaf2e2921b6450cedf339854c0902eb
Laurent Mwema <laumwema733@gmail.com>
2026-06-27 14:16:09 +0930

    Day 33:connect backend to frontend handle error

--- backend.py
+++ backend.py
@@ -1,22 +1,23 @@
 import requests
 
 API_KEY = 'c9d83d125dcfcfe04341301f58cd9d5d'
-def get_data(place,forecast_days=None, kind=None):
-    url =f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    filtered_data = data['list']
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
+def get_data(place,forecast_days=None):
 
-    return filtered_data
+        url =f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            filtered_data = data['list']
+            nr_values = 8 * forecast_days
+            filtered_data = filtered_data[:nr_values]
+            return filtered_data
+        else:
+            return 'please enter a valid place'
+
+
 
 
 
 
 if __name__=='__main__':
-    print(get_data('London',2,'Temperature'))
+    print(get_data('London',2))
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
+
+
+# Add Title,text input,slider,selectbox, and slider
 st.title("Weather Forecast for the Next Days")
-city = st.text_input("Place:",placeholder="Enter the name of the city")
+city = st.text_input("Place:",placeholder="Enter the name of the city" ,value='London')
 days = st.slider("Forecast Days:",min_value=1,max_value=5,value=1, help="How many days do you want to forecast")
-options = st.selectbox('Select data to view', ('temperature','sky' ))
+options = st.selectbox('Select data to view', ('Temperature','Sky' ))
 
 st.subheader(f'Temperature for the next {days} days in {city}')
+# Get the Temperature/Sky data
+filtered_data= get_data(city.title(),days )
 
-x,t = get_data(city,days ,options)
+if type(filtered_data) == str:
+    st.error(filtered_data)
+else :
+        if options == 'Temperature':
+            # create temperature plot
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+
+            figure = px.line(x=dates,y=temperatures,labels={'x':'Data','y':'Temperature (C)'})
+            st.plotly_chart(figure)
+        if options == 'Sky':
+            images ={'Clear':'images/clear.png', 'Clouds':'images/cloud.png', 'Rain':'images/rain.png', 'Snow':'images/snow.png'}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths,width=110)
 
 
-figure = px.line(x=d,y=t,labels={'x':'Data','y':'Temperature (C)'})
-st.plotly_chart(figure)
